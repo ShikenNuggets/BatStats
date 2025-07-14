504b2e28970becc8c2420697c0b28b000cca3f6b
ShikenNuggets <carter.rennick@gmail.com>
2025-07-13 20:40:25 -0400

    Quick test backend that returns a random number

--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Json, Router,
+};
+
+use rand::Rng;
+use serde::Serialize;
+use std::net::SocketAddr;
+use tower_http::cors::{Any, CorsLayer };
+use tokio::net::TcpListener;
+
+#[derive(Serialize)]
+struct RandomNumber{
+    value: u32,
+}
+
+async fn get_random_number() -> Json<RandomNumber> {
+    let random_value = rand::thread_rng().gen_range(1..=100);
+    Json(RandomNumber { value: random_value })
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/random", get(get_random_number))
+        .layer(CorsLayer::new().allow_origin(Any));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Backend listening on https://{}", addr);
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    println!("Listening on https://{}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
 }
