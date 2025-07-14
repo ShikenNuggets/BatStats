171d211e711c10bb9adfe5f28fc05b10fef677f2
ShikenNuggets <carter.rennick@gmail.com>
2025-07-13 21:03:39 -0400

    Fixes

--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -1,12 +1,11 @@
 use axum::{
-    routing::get,
-    Json, Router,
+    http::{HeaderValue, Method}, routing::get, Json, Router
 };
 
 use rand::Rng;
 use serde::Serialize;
 use std::net::SocketAddr;
-use tower_http::cors::{Any, CorsLayer };
+use tower_http::cors::{Any, Cors, CorsLayer };
 use tokio::net::TcpListener;
 
 #[derive(Serialize)]
@@ -21,15 +20,19 @@ async fn get_random_number() -> Json<RandomNumber> {
 
 #[tokio::main]
 async fn main() {
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET]);
+
     let app = Router::new()
         .route("/random", get(get_random_number))
-        .layer(CorsLayer::new().allow_origin(Any));
+        .layer(cors);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("Backend listening on https://{}", addr);
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
+    println!("Backend listening on http://{}", addr);
 
-    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    println!("Listening on https://{}", listener.local_addr().unwrap());
+    let listener = TcpListener::bind("127.0.0.1:3001").await.unwrap();
+    println!("Listening on http://{}", listener.local_addr().unwrap());
 
     axum::serve(listener, app).await.unwrap();
 }
