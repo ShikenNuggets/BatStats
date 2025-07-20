9965f289ad20b58a679cfa9195408a35b20ea6af
ShikenNuggets <carter.rennick@gmail.com>
2025-07-20 08:47:31 -0400

    Improvements

--- Backend/src/speedrun_api/http_utils.rs
+++ Backend/src/speedrun_api/http_utils.rs
@@ -2,22 +2,27 @@ use reqwest;
 use url::Url;
 use std::{collections::HashMap, error::Error};
 
-pub async fn get_http_result(url: Url) -> Result<(), Box<dyn Error>>{
+async fn get_http_result_internal(url: Url) -> Result<String, Box<dyn Error>>{
 	let response = reqwest::get(url).await?;
+	let response = response.error_for_status()?;
 	let body = response.text().await?;
-
-	println!("Response Body: \n{}", body);
-	Ok(())
+	Ok(body)
 }
 
-pub fn parse_to_url(base_url: &str, args: HashMap<&str, &str>) -> Url{
-	let mut url = Url::parse(&base_url).unwrap();
+pub fn parse_to_url(base_url: &str, args: HashMap<&str, &str>) -> Result<Url, Box<dyn Error>>{
+	let mut url = Url::parse(&base_url)?;
 	url.query_pairs_mut().extend_pairs(args.iter());
-	return url;
+	Ok(url)
 }
 
-pub async fn get_http_result_with_args(base_url: &str, args: HashMap<&str, &str>) -> Result<(), Box<dyn Error>>{
-	let final_url = parse_to_url(base_url, args);
-	get_http_result(final_url).await?;
-	Ok(())
+pub async fn get_http_result(url: &str) -> Result<String, Box<dyn Error>>{
+	let url = Url::parse(url)?;
+	let result = get_http_result_internal(url).await?;
+	Ok(result)
+}
+
+pub async fn get_http_result_with_args(base_url: &str, args: HashMap<&str, &str>) -> Result<String, Box<dyn Error>>{
+	let final_url = parse_to_url(base_url, args)?;
+	let result = get_http_result_internal(final_url).await?;
+	Ok(result)
 }
