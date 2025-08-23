b470854ed8bde7d88b9c6186d68e05dcca2900a8
ShikenNuggets <carter.rennick@gmail.com>
2025-08-23 15:09:10 -0400

    Log when the same URL is requested multiple times

--- Backend/src/speedrun_api/http_utils.rs
+++ Backend/src/speedrun_api/http_utils.rs
@@ -1,8 +1,18 @@
+use dashmap::DashSet;
+use once_cell::sync::Lazy;
 use reqwest;
 use url::Url;
-use std::{collections::HashMap, error::Error};
+use std::{collections::{HashMap}, error::Error};
+
+static URL_REQUESTS: Lazy<DashSet<Url>> = Lazy::new(DashSet::new);
 
 async fn get_http_result_internal(url: Url) -> Result<String, Box<dyn Error>>{
+	if URL_REQUESTS.contains(&url){
+		println!("Duplicate request \"{}\" detected!", url);
+	}else{
+		URL_REQUESTS.insert(url.clone());
+	}
+
 	let response = reqwest::get(url).await?;
 	let response = response.error_for_status()?;
 	let body = response.text().await?;
